cae0bfb5fbcdaa751d9d6bb4163cf7581fbd404b
Fabrizio951 <fabrizio_zuniga2003@hotmail.com>
2024-05-07 17:42:44 -0500

    A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,45 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aprender';
+  title = 'Nigger';
+  tula = "Negroide";
+
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio='http://www.google.com';
+
+  articulos = [{
+    codigo: 1,
+    descripcion: 'naranjas',
+    precio: 540
+  },{
+    codigo: 2,
+    descripcion: 'manzanas',
+    precio: 900
+  },{
+    codigo: 3,
+    descripcion: 'peras',
+    precio: 490
+  }];
+
+  generarNumero() {
+    return Math.floor(Math.random() * 3) + 1;
+  }
+
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++)
+      suma += this.sueldos[x];
+    return suma;
+  }
 }
